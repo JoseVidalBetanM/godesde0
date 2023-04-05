13907116325f952ad3372d9be3d4b062fb07e919
JoseVidalBetanM <josevidalbetanm@gmail.com>
2023-04-05 17:24:57 -0300

    siguiente commit

--- ejer_interfaces/interfaces.go
+++ ejer_interfaces/interfaces.go
@@ -9,28 +9,47 @@ import (
 	"github.com/JoseVidalBetanM/godesde0/interfaces"
 )
 
+var num1 int
+var num2 int
+var num3 int
+var nombre string
+var err error
+
 func HumanosRespirando(hu interfaces.Humano) {
 	scanner := bufio.NewScanner(os.Stdin)
-	hu.Respirar()
-	fmt.Printf("Soy un/a %s, y estoy respirando \n", hu.Sexo())
+
+	fmt.Println("Nombre: ")
+	if scanner.Scan() {
+		nombre = scanner.Text()
+
+	}
 
 	fmt.Println("Edad: ")
 	if scanner.Scan() {
-		num1, _ := strconv.Atoi(scanner.Text())
-		fmt.Println(num1)
+		num1, err = strconv.Atoi(scanner.Text())
+		if err != nil {
+			panic("hubo un error " + err.Error())
+		}
 	}
 
 	fmt.Println("Altura: ")
 	if scanner.Scan() {
-		num2, _ := strconv.Atoi(scanner.Text())
-		fmt.Println(num2)
+		num2, err = strconv.Atoi(scanner.Text())
+		if err != nil {
+			panic("hubo un error " + err.Error())
+		}
 	}
 
 	fmt.Println("Peso(Kg): ")
 	if scanner.Scan() {
-		num3, _ := strconv.Atoi(scanner.Text())
-		fmt.Println(num3)
+		num3, err = strconv.Atoi(scanner.Text())
+		if err != nil {
+			panic("hubo un error " + err.Error())
+		}
 
 	}
+
+	hu.Respirar()
+	fmt.Printf("Soy un/a %s, me llamo %s , tengo %d años de edad, mido %d de altura, peso %d y estoy respirando \n", hu.Sexo(), nombre, num1, num2, num3)
 
 }
