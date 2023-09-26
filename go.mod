ec93286ee970a547d511ba0827f0d282e6a51281
JoseVidalBetanM <josevidalbetanm@gmail.com>
2023-09-26 16:07:33 -0300

    primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JoseVidalBetanM/godesde0
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/JoseVidalBetanM/godesde0/variables"
+
+func main() {
+	variables.Muestroenteros()
+}
