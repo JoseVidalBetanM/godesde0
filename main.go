f1da6b54cc5e82101c11aa529ba53db1b929c5fd
JoseVidalBetanM <josevidalbetanm@gmail.com>
2023-10-14 13:21:13 -0300

    siguiente commit

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/JoseVidalBetanM/godesde0/variables"
+	"runtime"
+	//"github.com/JoseVidalBetanM/godesde0/variables"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(100)
+	/*estado, texto := variables.ConviertoaTexto(100)
 	fmt.Println(estado)
-	fmt.Println(texto)
+	fmt.Println(texto)*/
+	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+		fmt.Println("esto no es windows")
+	} else {
+		fmt.Println("esto es windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "Linux:":
+		fmt.Println("esto es linux")
+	case "Darwin":
+		fmt.Println("esto es darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 
 }
